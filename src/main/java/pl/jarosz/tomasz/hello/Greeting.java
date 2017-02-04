28e3fda47ee2d08de0e24206651e1c01b68c19c2
Tomasz Jarosz <tomaszjarosz1994@gmail.com>
2017-02-04 20:27:25 +0100

    init commit - setup angular2 and spring boot, build app to one war

---
+++ src/main/java/pl/jarosz/tomasz/hello/Greeting.java
@@ -0,0 +1,23 @@
+package pl.jarosz.tomasz.hello;
+
+/**
+ * Created by Tomek on 04.02.2017.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
